77c0a65d1b408610fb721e562c9fcd869a75d2e8
Thanh Tuấn <154106060+tuanthanh2005@users.noreply.github.com>
2026-01-23 09:39:44 +0700

    a

--- app/Http/Controllers/SitemapController.php
+++ app/Http/Controllers/SitemapController.php
@@ -13,7 +13,11 @@ class SitemapController extends Controller
     {
         $products = Product::all();
         $blogs = Blog::all();
-        $tiktokDeals = TiktokDeal::where('is_active', true)->get();
+        
+        // Lấy danh mục sản phẩm unique
+        $productCategories = Product::select('category')->distinct()->pluck('category');
+        // Lấy danh mục blog unique
+        $blogCategories = Blog::select('category')->distinct()->pluck('category');
 
         $xml = '<?xml version="1.0" encoding="UTF-8"?>';
         $xml .= '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">';
@@ -34,6 +38,18 @@ class SitemapController extends Controller
         $xml .= '<priority>0.9</priority>';
         $xml .= '</url>';
         
+        // Product Categories
+        foreach ($productCategories as $category) {
+            if ($category) {
+                $xml .= '<url>';
+                $xml .= '<loc>' . route('shop') . '?category=' . urlencode($category) . '</loc>';
+                $xml .= '<lastmod>' . date('Y-m-d') . '</lastmod>';
+                $xml .= '<changefreq>daily</changefreq>';
+                $xml .= '<priority>0.85</priority>';
+                $xml .= '</url>';
+            }
+        }
+        
         // Products
         foreach ($products as $product) {
             $xml .= '<url>';
@@ -52,29 +68,24 @@ class SitemapController extends Controller
         $xml .= '<priority>0.8</priority>';
         $xml .= '</url>';
         
+        // Blog Categories
+        foreach ($blogCategories as $category) {
+            if ($category) {
+                $xml .= '<url>';
+                $xml .= '<loc>' . route('blog.category', $category) . '</loc>';
+                $xml .= '<lastmod>' . date('Y-m-d') . '</lastmod>';
+                $xml .= '<changefreq>daily</changefreq>';
+                $xml .= '<priority>0.75</priority>';
+                $xml .= '</url>';
+            }
+        }
+        
         // Blogs
         foreach ($blogs as $blog) {
             $xml .= '<url>';
             $xml .= '<loc>' . route('blog.show', $blog->slug) . '</loc>';
             $xml .= '<lastmod>' . $blog->updated_at->format('Y-m-d') . '</lastmod>';
             $xml .= '<changefreq>weekly</changefreq>';
-            $xml .= '<priority>0.7</priority>';
-            $xml .= '</url>';
-        }
-        
-        // TikTok Deals
-        $xml .= '<url>';
-        $xml .= '<loc>' . route('tiktok-deals.index') . '</loc>';
-        $xml .= '<lastmod>' . date('Y-m-d') . '</lastmod>';
-        $xml .= '<changefreq>daily</changefreq>';
-        $xml .= '<priority>0.8</priority>';
-        $xml .= '</url>';
-        
-        foreach ($tiktokDeals as $deal) {
-            $xml .= '<url>';
-            $xml .= '<loc>' . route('tiktok-deals.show', $deal->slug) . '</loc>';
-            $xml .= '<lastmod>' . $deal->updated_at->format('Y-m-d') . '</lastmod>';
-            $xml .= '<changefreq>daily</changefreq>';
             $xml .= '<priority>0.7</priority>';
             $xml .= '</url>';
         }

cdb799d7cb7dd178a9ebba27bb484ef60f1903a8
Thanh Tuấn <154106060+tuanthanh2005@users.noreply.github.com>
2026-01-24 14:29:49 +0700

    s

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -20,8 +20,8 @@ class HomeController extends Controller
         // Lấy deals Tiktok nổi bật (4 items)
         $tiktokDeals = TiktokDeal::featured()->active()->ordered()->take(4)->get();
         
-        // Lấy 3 blog nổi bật mới nhất
-        $latestBlogs = Blog::featured()->latestPosts(3)->get();
+        // Lấy 3 blog mới nhất (published)
+        $latestBlogs = Blog::published()->orderBy('published_at', 'desc')->take(3)->get();
         
         return view('home', compact('featuredProducts', 'highlightProducts', 'tiktokDeals', 'latestBlogs'));
     }

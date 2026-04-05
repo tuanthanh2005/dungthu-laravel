b8de4ead6e3d16035506a5c77f9ce0facc6d6486
Thanh Tuấn <154106060+tuanthanh2005@users.noreply.github.com>
2026-04-06 00:12:13 +0700

    bỏ boxchat

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -75,9 +75,6 @@
     <!-- Footer -->
     @include('partials.footer')
 
-    <!-- Chat Widget -->
-    @include('partials.chat')
-
     <!-- Scripts -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
     <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
